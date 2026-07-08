f5f5cad993b902684a572e01b4effaad79beb979
notsolittleellie <akuzikenkhono5@gmail.com>
2026-07-08 12:49:37 +0200

    added in equipment

--- script.js
+++ script.js
@@ -1,20 +1,9 @@
-let slideIndex = 0;
-showSlides();
-
-function showSlides() {
-    let slides = document.getElementsByClassName("slide");
-
-    for (let i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
+function closeModel() {
+    document.getElementById("welcomemodel").style.display = "none";
+}
+window.onclick = function(event) {
+    let model = document.getElementById("welcomemodel");
+    if (event.target == model) {
+        model.style.display = "none";
     }
-
-    slideIndex++;
-
-    if (slideIndex > slides.length) {
-        slideIndex = 1;
-    }
-
-    slides[slideIndex - 1].style.display = "block";
-
-    setTimeout(showSlides, 3000);
 }
